6a99bd976eb71dfcfec7e298a6c036052cca1e88
unknown <huynhtanphat130201@gmail.com>
2023-05-12 18:54:09 +0700

    #tanphat update push-image.sh

--- push-image.sh
+++ push-image.sh
@@ -4,7 +4,7 @@
 set -eu
 
 # login to your docker hub account
-docker login --email $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSPWORD
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSPWORD
 
 # use the docker tag command to give the image a new name
 docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
